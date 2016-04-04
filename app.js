ef387031b43d1666f7717a6d4a26fa2de69e36da
Caleb Kirksey <ckirksey3@gmail.com>
2016-04-03 21:59:38 -0400

    attempt to fix syntax error in app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var redis = require('redis');
 var url = require('url');
 var redisURL = url.parse(process.env.REDISCLOUD_URL); //Heroku Redis
 var speechHandler = require('./speech_handler.js')
-var speechHandlerInstance = new SpeechHandler()
+var speechHandlerInstance = new speechHandler()
 var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
 client.auth(redisURL.auth.split(":")[1]);
 
